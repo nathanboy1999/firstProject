2e79acc9631b823cb142f756648823671f7a3a6b
Nate <11088@bwls.net>
2016-10-13 15:17:04 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,4 +6,16 @@ function setup() {
 function draw() {
   rect( mouseX , mouseY , 110 , 110 );
   fill(15,75,15)
+  var value = 0;
+function draw() {
+  fill(value);
+  rect(25, 25, 50, 50);
+}
+function mouseClicked() {
+  if (value == 0) {
+    value = 255;
+  } else {
+    value = 0;
+  }
+}
 }
